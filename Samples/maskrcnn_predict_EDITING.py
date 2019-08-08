1719f9a18e044c7a14a4f0ea73dce32d4b238189
hibajamal <35984866+hibajamal@users.noreply.github.com>
2019-08-08 16:02:16 +0500

    Update maskrcnn_predict_EDITING.py

--- Samples/maskrcnn_predict_EDITING.py
+++ Samples/maskrcnn_predict_EDITING.py
@@ -12,7 +12,8 @@ import imutils
 import random
 import cv2
 import os
-import copy
+import time
+from PIL import Image
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -27,21 +28,10 @@ args = vars(ap.parse_args())
 # load the class label names from disk, one label per line
 CLASS_NAMES = open(args["labels"]).read().strip().split("\n")
 
+start = time.time()
+
 # generate random (but visually distinct) colors for each class label
 # (thanks to Matterport Mask R-CNN for the method!)
-
-
-'''
-	THESE NEXT 4 LINES ARE IN CHARGE OF MASK COLORS
-'''
-
-
-'''the next line for hsv has to be changed for colors to become binary for our TWO classifiers
-hsv = [(i / len(CLASS_NAMES), 1, 1.0) for i in range(len(CLASS_NAMES))]
-COLORS = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
-the next two lines should be omitted 
-random.seed(42)
-random.shuffle(COLORS)'''
 
 class SimpleConfig(Config):
 	# give the configuration a recognizable name
@@ -77,10 +67,8 @@ image = imutils.resize(image, width=512)
 print("[INFO] making predictions with Mask R-CNN...")
 r = model.detect([image], verbose=1)[0]
 
-masked_image = copy.deepcopy(image) # added
-cv2.namedWindow("HumanDetected") # added
-cv2.namedWindow("HumanMask") # added
-mask = None # create empty variable to store mask of human detected
+# create empty variable to store mask of human detected
+mask = None
 
 # loop over of the detected object's bounding boxes and masks
 for i in range(0, r["rois"].shape[0]):
@@ -88,47 +76,34 @@ for i in range(0, r["rois"].shape[0]):
 	# grab the color to visualize the mask (in BGR format)
 	classID = r["class_ids"][i]
 	mask = r["masks"][:, :, i]
-	print(type(mask), mask.shape, mask.dtype)
 
 	if CLASS_NAMES[classID] == "person":
 		color = (0, 0, 0)
+		# first person detected is our target
+		break
 
-		# visualize the pixel-wise mask of the object
-		# image = visualize.apply_mask(image, mask, color, alpha=1)
-		masked_image = visualize.apply_mask(masked_image, mask, color, alpha=1)
-		print(masked_image.shape, type(masked_image))
+# adding alpha channel
+image = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
+# converting numpy image to pillow image
+pil_image = Image.fromarray(image)
+# creating new pil image for png
+qhuman_mask = Image.new(pil_image.mode, pil_image.size)
+# loading both images
+new_pixels = qhuman_mask.load()
+original_pixels = pil_image.load()
 
-		(startY, startX, endY, endX) = r["rois"][i] # added
-		masked_image = masked_image[startY:endY, startX:endX]
-		break # added till here
+# create the PNG image pixel by pixel
+for i in range(qhuman_mask.size[0]):
+	for j in range(qhuman_mask.size[1]):
+		if mask[j][i]:
+			new_pixels[i, j] = original_pixels[i, j]
+		else:
+			new_pixels[i, j] = (0, 0, 0, 0)
 
-# convert the image back to BGR so we can use OpenCV's drawing
-# functions
-image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-masked_image = cv2.cvtColor(masked_image, cv2.COLOR_RGB2GRAY) # added
-masked_image = cv2.bitwise_not(masked_image)
+qhuman_mask.save("out.png")
+qhuman_mask.close()
 
-# loop over the predicted scores and class labels
-'''
-for i in range(0, len(r["scores"])):
-	# extract the bounding box information, class ID, label, predicted
-	# probability, and visualization color
-	(startY, startX, endY, endX) = r["rois"][i]
-	classID = r["class_ids"][i]
-	label = CLASS_NAMES[classID]
-	score = r["scores"][i]
-	color = [int(c) for c in np.array(COLORS[classID]) * 255]
+print()
+print(time.time()-start, "seconds taken")
 
-	# draw the bounding box, class label, and score of the object
-	cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
-	text = "{}: {:.3f}".format(label, score)
-	y = startY - 10 if startY - 10 > 10 else startY + 10
-	cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX,
-		0.6, color, 2)
-'''
-
-# show the output image
-# cv2.imshow("Output", image)
-cv2.imshow("Input", image) # added
-cv2.imshow("HumanDetected", masked_image) #added
 cv2.waitKey()
